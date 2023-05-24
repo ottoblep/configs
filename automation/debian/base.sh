c0b539a473d5a0392cf93c510811f5b5e707affc
Severin Lochschmidt <seviron53@gmail.com>
2023-05-24 12:52:44 +0200

    Fix headless zsh install

--- automation/debian/base.sh
+++ automation/debian/base.sh
@@ -1,10 +1,10 @@
 # Depends on debian version
 echo "Updating sources..."
-sudo echo "deb http://http.us.debian.org/debian stable contrib non-free" | sudo tee -a /etc/apt/sources.list
-sudo echo "deb-src http://http.us.debian.org/debian stable contrib non-free" | sudo tee -a /etc/apt/sources.list
-sudo echo "deb http://deb.debian.org/debian bullseye-backports main contrib non-free" | sudo tee -a /etc/apt/sources.list
+sudo echo "deb http://deb.debian.org/debian stable contrib non-free" | sudo tee -a /etc/apt/sources.list
+sudo echo "deb-src http://deb.debian.org/debian stable stable contrib non-free" | sudo tee -a /etc/apt/sources.list
 sudo echo "deb http://deb.debian.org/debian bullseye-security main" | sudo tee -a /etc/apt/sources.list
-sudo echo "deb http://deb.debian.org/debian bullseye-updates main" | sudo tee -a /etc/apt/sources.list
+ sudo echo "deb http://deb.debian.org/debian bullseye-updates main" | sudo tee -a /etc/apt/sources.list
+# sudo echo "deb http://deb.debian.org/debian stable bullseye-backports main contrib non-free" | sudo tee -a /etc/apt/sources.list
 
 echo "Updating package database..."
 sudo apt update -y
@@ -22,7 +22,7 @@ yt-dlp
 # Enable zsh
 chsh -s $(which zsh)
 # Install OhMyZsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh) --unattended" 
 
 echo "Importing configs..."
 cp -rT ~/configs/home ~
